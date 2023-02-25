b1465475b9d03f1640b7111e3b236a6b7f9c9d38
dobroStas <Konanykhin.Stanislav@gmail.ru>
2023-02-25 21:28:24 +0300

    Init commit. Ex.1

---
+++ Ex.1/Program.cs
@@ -0,0 +1,16 @@
+﻿//Напишите программу, которая на вход принимает число и выдает его квадрат (число умноженное
+//на само себя).
+
+//Например:
+//4 -> 16
+//-3 -> 9
+//-7 -> 49
+
+int number = Convert.ToInt32(Console.ReadLine());
+number = number * number;
+Console.WriteLine("Your resalt is : " + number);
+//or
+//Console.WriteLine("Your {1} resalt is : {0}", number, text);
+//or
+//Console.WriteLine($"YoYour resalt is : " {function(formula))})
+
